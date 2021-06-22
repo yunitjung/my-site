ebc1fef1bb673dff58e39535270376e8e8bb56a4
Yuni Tjung <tjungyuni@gmail.com>
2021-06-22 16:09:19 +0700

    update config

--- gatsby-config.js
+++ gatsby-config.js
@@ -3,5 +3,4 @@ module.exports = {
     title: "Yuni Tjung",
   },
   plugins: ["gatsby-plugin-gatsby-cloud", 'gatsby-plugin-postcss', 'gatsby-plugin-image', 'gatsby-plugin-sharp'],
-  pathPrefix: "/yunitjung.github.io",
 };
