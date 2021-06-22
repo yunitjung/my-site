2f1b585224575b5bac0980624554a4f931a1dac6
Yuni Tjung <tjungyuni@gmail.com>
2021-06-22 18:16:48 +0700

    fix minor

--- src/components/layout.js
+++ src/components/layout.js
@@ -5,8 +5,6 @@ import { FaGithub } from 'react-icons/fa';
 import { FaLinkedinIn } from 'react-icons/fa';
 import { FaTwitter } from 'react-icons/fa';
 
-const menuMobile = 'hidden';
-
 const Layout = ({pageTitle, children}) => {
     return (
         <div className="bg-indigo-500 min-h-screen max-w-screen">
@@ -33,7 +31,7 @@ const Layout = ({pageTitle, children}) => {
                 <button
                   type="button"
                   className="bg-yellow-50 bg-opacity-25 inline-flex items-center justify-center p-2 rounded-md text-gray-500 hover:text-white focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-offset-gray-800 focus:ring-white"
-                  aria-controls="mobile-menu"
+                  // aria-controls="mobile-menu"
                   aria-expanded="false"
                 >
                   <span className="sr-only">Open main menu</span>
@@ -47,9 +45,9 @@ const Layout = ({pageTitle, children}) => {
                     aria-hidden="true"
                   >
                     <path
-                      stroke-linecap="round"
-                      stroke-Linejoin="round"
-                      stroke-width="2"
+                      strokeLinecap="round"
+                      strokeLinejoin="round"
+                      strokeWidth="2"
                       d="M4 6h16M4 12h16M4 18h16"
                     />
                   </svg>
@@ -63,9 +61,9 @@ const Layout = ({pageTitle, children}) => {
                     aria-hidden="true"
                   >
                     <path
-                      stroke-linecap="round"
-                      stroke-linejoin="round"
-                      stroke-width="2"
+                      strokeLinecap="round"
+                      strokeLinejoin="round"
+                      strokeWidth="2"
                       d="M6 18L18 6M6 6l12 12"
                     />
                   </svg>
@@ -73,7 +71,7 @@ const Layout = ({pageTitle, children}) => {
               </div>
             </div>
           </div>
-          <div className="hidden md:hidden" id="mobile-menu">
+          <div className="md:hidden" id="mobile-menu">
             <div className="px-2 pt-2 pb-3 space-y-1 sm:px-3">
               <Link to ="/"
                 className="text-gray-300 hover:text-gray-500 block px-3 py-2 rounded-md text-base font-medium"
