7441fa0c55cd7980ad85127ee150fc29a3fa3501
Yuni Tjung <tjungyuni@gmail.com>
2021-06-22 16:04:28 +0700

    update config for deploying gatsby

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,6 +1,7 @@
 module.exports = {
   siteMetadata: {
-    title: "My Site",
+    title: "Yuni Tjung",
   },
   plugins: ["gatsby-plugin-gatsby-cloud", 'gatsby-plugin-postcss', 'gatsby-plugin-image', 'gatsby-plugin-sharp'],
+  pathPrefix: "/yunitjung.github.io",
 };
