b0067e48912053356c3c62ecdb16ebd117cb4e14
Yuni Tjung <tjungyuni@gmail.com>
2021-08-15 13:34:30 +0700

    fix theme dark

--- gatsby-config.js
+++ gatsby-config.js
@@ -7,20 +7,5 @@ module.exports = {
     "gatsby-plugin-postcss",
     "gatsby-plugin-sharp",
     "@chakra-ui/gatsby-plugin",
-    {
-      resolve: "@chakra-ui/gatsby-plugin",
-      options: {
-        /**
-         * @property {boolean} [resetCSS=true]
-         * if false, this plugin will not use `<CSSReset />
-         */
-        resetCSS: true,
-        /**
-         * @property {boolean} [isUsingColorMode=true]
-         * if false, this plugin will not use <ColorModeProvider />
-         */
-        isUsingColorMode: true,
-      },
-    },
   ],
 };
--- src/components/layouts.js
+++ src/components/layouts.js
@@ -1,9 +1,10 @@
 import React from "react";
 import { ChakraProvider, Box } from "@chakra-ui/react";
+import theme from "../@chakra-ui/gatsby-plugin/theme";
 
 function Layout({ children, variant = "regular" }) {
   return (
-    <ChakraProvider resetCSS="true">
+    <ChakraProvider resetCSS="true" theme={theme}>
       <Box
         mt={8}
         mb={8}
